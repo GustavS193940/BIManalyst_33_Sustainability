4d9463c81bddb67b8b29c3369ba6d59f7ab2afa4
GustavS193940 <s193940@student.dtu.dk>
2025-09-22 16:34:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import ifcopenshell
 
 from rules import NFARule
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open(r"C:\Users\gusta\Desktop\25-16-D-ARCH.ifc")
 
 NFAResult = NFARule.checkRule(model)
 
-print("NFA result: The net floor area in the model is", NFAResult,"m²")
+print("NFA result: The net floor area in the model is", round(NFAResult,0),"m²")
